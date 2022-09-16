dd92699526d901c055cf148bab57dd8d6b77b24d
tonyatrushkova <antonina.trushkova@gmail.com>
2022-09-16 22:44:37 +0300

    If else condition

---
+++ Example_1/Example_5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Type name ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Nice to see you Masha!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
